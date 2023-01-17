796ff26ad1cab34861c8c21fad360bc33458d506
Kierandoolan <kierandoolan3@gmail.com>
2023-01-17 12:56:43 +0000

    add admin superuser and add classes to admin.py

--- auto/admin.py
+++ auto/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import CarAd, Comment
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+
+
+@admin.register(CarAd)
+class CarAdAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'author', 'created_on', 'year', 'nct', 'price')
+    list_filter = ('author', 'created_on') 
+    summernote_fields = ('description',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
