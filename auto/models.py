c32778122d21c2961479bd1713c8f6a814fafe21
Kierandoolan <kierandoolan3@gmail.com>
2023-01-17 11:53:43 +0000

    add models in model.py

--- auto/models.py
+++ auto/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class CarAd(models.Model):
+    """
+    A model to post an ad for a car
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="auto_CarAds")
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now=True)
+    description = models.TextField(max_length=500)
+    number = models.IntegerField(null=True)
+    image = models.ImageField()
+    price = models.IntegerField(null=True)
+    year = models.DateField(auto_now=False)
+    nct = models.DateField(auto_now=False)
+    likes = models.ManyToManyField(
+        User, related_name='autopost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(CarAd, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
+
