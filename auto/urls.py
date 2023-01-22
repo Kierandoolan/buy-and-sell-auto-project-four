b47d1873fb9de24a136d5da5a97babfa3c2c5ecb
Kierandoolan <kierandoolan3@gmail.com>
2023-01-22 16:00:53 +0000

    add new url and ad_detail.html

--- auto/urls.py
+++ auto/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.CarAdList.as_view(), name='home')
+    path('', views.CarAdList.as_view(), name='home'),
+    path('<slug:slug>/', views.AdDetail.as_view(), name='ad_detail'),
 ]
