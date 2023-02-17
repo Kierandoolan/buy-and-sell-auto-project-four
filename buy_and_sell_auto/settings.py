f03200bde8fd4ed8164e0fb7e12ed3dfb3656774
Kierandoolan <kierandoolan3@gmail.com>
2023-02-17 08:16:14 +0000

    tidy up css and fix password validator

--- buy_and_sell_auto/settings.py
+++ buy_and_sell_auto/settings.py
@@ -139,36 +139,32 @@ GRAPH_MODELS = {
     ],
 }
 
-CONTACT_EMAIL = 'buyandsellauto1234@gmail.com'
-ADMIN_EMAIL = ['buyandsellauto1234@gmail.com']
+# CONTACT_EMAIL = 'buyandsellauto1234@gmail.com'
+# ADMIN_EMAIL = ['buyandsellauto1234@gmail.com']
 
 
 # Twilio SendGrid
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 2525
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_PORT = 2525
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation\
-            .UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation\
-            .MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation\
-            .CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation\
-            .NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
